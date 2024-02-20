24c2f2112c94791a78940849b9dbaf6aff9e0fbb
peanutbuttermurmite <73216267+peanutbuttermurmite@users.noreply.github.com>
2024-02-20 14:35:09 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-chmod +x ./proot
-mkdir /tmp/opt
-mkdir /tmp/bin
-./proot -S assets bash assets/etc/skel/.bashrc -b /tmp/opt:/opt /tmp/bin:/usr/local/bin
+chmod u+x proot
+
+./proot -S assets/
